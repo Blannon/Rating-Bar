e8109e76ffa69f89877700f7c23a601933c5d20a
Blannon <63664876+Blannon@users.noreply.github.com>
2023-08-18 03:58:36 +0300

    Rating Bar

---
+++ app/src/main/java/com/blannoncodes/ratinngbar/MainActivity.java
@@ -0,0 +1,34 @@
+package com.blannoncodes.ratinngbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    RatingBar ratingBar;
+    Button btn_submit;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ratingBar = findViewById(R.id.ratingbar);
+        btn_submit = findViewById(R.id.btn_submit);
+
+        btn_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String stars = String.valueOf(ratingBar.getRating());
+                Toast.makeText(getApplicationContext(), stars+ "Stars", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
